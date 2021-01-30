23fc16012450bda8b25bdd82342059d7b06fae5c
sto17001 <sto17001@byui.edu>
2021-01-30 15:09:27 -0700

    update

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -6,5 +6,5 @@ let d = new Date(document.lastModified);
 document.getElementById("lastupdate").textContent = d;
 
 function toggleMenu(){
-    document.getElementById("primaryNav").classList.toggle("hide");
+    document.getElementById("nav-menu").classList.toggle("hide");
   }
