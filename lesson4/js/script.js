677a06945b103cae93e85a9d1e57132c4692c23f
sto17001 <sto17001@byui.edu>
2021-01-30 15:15:27 -0700

    update

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,6 +1,5 @@
 let b = new Date();
 let year = b.getFullYear();
-document.getElementById("currentyear").textContent = year;
 
 let d = new Date(document.lastModified);
 document.getElementById("lastupdate").textContent = d;
