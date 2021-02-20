3d9f2333870c05c2cab9ffb7541b38328fc400b3
sto17001 <sto17001@byui.edu>
2021-02-20 13:35:02 -0700

    update

--- lesson7/js/imagehandling.js
+++ lesson7/js/imagehandling.js
@@ -6,12 +6,6 @@ const loadImages = (image) => {
   };
 };
 
-const imgOptions = {
-  threshold: 1,
-  rootMargin: '0px 0px 50px 0px'
-};
-
-
 if('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
       items.forEach((item) => {
