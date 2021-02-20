88aff6ba5633c9ab7063d9ee74f3da8862ee8731
sto17001 <sto17001@byui.edu>
2021-02-20 13:25:09 -0700

    update

--- lesson7/js/imagehandling.js
+++ lesson7/js/imagehandling.js
@@ -1,27 +1,26 @@
-const imagesToLoad = document.querySelectorAll('img[data-src]');
-
+let imagesToLoad = document.querySelectorAll('img[data-src]');
 const loadImages = (image) => {
-  image.setAttribute('src', image.getAtrribute('data-src'));
+  image.setAttribute('src', image.getAttribute('data-src'));
   image.onload = () => {
     image.removeAttribute('data-src');
   };
 };
 
 
-if ('IntersectionObserver' in window) {
-  const imgObserver = new IntersectionObserver((items) => {
-    items.forEach((item) => {
-      if (item.isIntersecting) {
-        loadImages(item.target);
-        imgObserver.unobserve(item.target);
-      }
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
     });
-  });
-  imagesToLoad.forEach((img) => {
-    imgObserver.observe(img);
-  });
-} else {
-  imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
-}
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
