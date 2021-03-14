9e0ae1df1bb2f7ad29adff71f8483aafde31e542
sto17001 <sto17001@byui.edu>
2021-03-13 18:27:20 -0700

    update

--- lesson10/js/forecastapi.js
+++ lesson10/js/forecastapi.js
@@ -8,14 +8,14 @@ fetch(apiURLforecast)
         jsObject.list.forEach(element => {
             if (element.dt_txt.includes("18:00:00")) {
                 let d = new Date(element.dt_txt);
-                document.getElementById("dayOfWeek${day}").textContent = dayOfWeek[d.getDay()];
+                document.getElementById('dayOfWeek${day}').textContent = dayOfWeek[d.getDay()];
 
-                document.getElementById("forecastTemperature${day}").textContent = element.main.temp;
+                document.getElementById('forecastTemperature${day}').textContent = element.main.temp;
 
-                let imagesrc = "https://openweathermap.org/img/wn/" + element.weather[0].icon + "@2x.png";
+                let imagesrc = `https://openweathermap.org/img/wn/${element.weather[0].icon}@2x.png`;
                 let imgalt = element.weather[0].description;
-                document.getElementById("icon${day}").setAttribute("src", imagesrc);
-                document.getElementById("icon${day}").setAttribute("alt", imgalt);
+                document.getElementById('icon${day}').setAttribute('src', imagesrc);
+                document.getElementById('icon${day}').setAttribute('alt', imgalt);
 
                 day++;
             }
