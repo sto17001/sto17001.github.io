41f133f3e72a557bb7ecac4c27d724b9c8aee67a
sto17001 <sto17001@byui.edu>
2021-03-13 18:41:22 -0700

    update

--- lesson10/js/forecastapi.js
+++ lesson10/js/forecastapi.js
@@ -2,22 +2,18 @@ const apiURLforecast = "https://api.openweathermap.org/data/2.5/forecast?id=5604
 fetch(apiURLforecast)
     .then((response) => response.json())
     .then((jsObject) => {
-        let day = 1;
-        const dayofWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+        let newList = jsObject.list.filter(x => x.dt_txt.includes("18:00:00"));
 
-        jsObject.list.forEach(element => {
-            if (element.dt_txt.includes("18:00:00")) {
-                let d = new Date(element.dt_txt);
-                document.getElementById('dayOfWeek${day}').textContent = dayofWeek[d.getDay()];
+        for (let day = 0; day <= 4; day++) {
+            let d = new Date(newList[day].dt_txt);
+            document.getElementById(`dayOfWeek${day+1}`).textContent = dayOfWeek[d.getDay()];
+            document.getElementById(`forecastTemperature${day+1}`).textContent = newList[day].main.temp;
 
-                document.getElementById('forecastTemperature${day}').textContent = element.main.temp;
+            const imgalt = newList[day].weather[0].description;
+            const imagsrc = 'https://openweathermap.org/img/wn/' + newList[day].weather[0].icon + '@2x.png';
+            document.getElementById(`icon${day+1}`).setAttribute('src', imagsrc);
+            document.getElementById(`icon${day+1}`).setAttribute('alt', imgalt);
+        }
 
-                let imagesrc = 'https://openweathermap.org/img/wn/' + element.weather[0].icon + '@2x.png';
-                let imgalt = element.weather[0].description;
-                document.getElementById('icon${day}').setAttribute('src', imagesrc);
-                document.getElementById('icon${day}').setAttribute('alt', imgalt);
-
-                day++;
-            }
-        });
     });
