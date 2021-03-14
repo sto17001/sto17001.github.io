d774a1e081ed5ba0c86389b71965ad95892a499d
sto17001 <sto17001@byui.edu>
2021-03-13 17:55:27 -0700

    update

---
+++ lesson10/js/forecastapi.js
@@ -0,0 +1,23 @@
+const apiURLforecast = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=bb98ec60049766ba796288b29310adc3&units=imperial";
+fetch(apiURLforecast)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        let day = 1;
+        const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+        jsObject.list.forEach(element => {
+            if (element.dt_txt.includes("18:00:00")) {
+                let d = new Date(element.dt_txt);
+                document.getElementById("dayOfWeek${day}").textContent = dayOfWeek[d.getDay()];
+
+                document.getElementById("forecastTemperature${day}").textContent = element.main.temp;
+
+                let imagesrc = "https://openweathermap.org/img/wn/" + element.weather[0].icon + "@2x.png";
+                let imgalt = element.weather[0].description;
+                document.getElementById("icon${day}").setAttribute("src", imagesrc);
+                document.getElementById("icon${day}").setAttribute("alt", imgalt);
+
+                day++;
+            }
+        });
+    });
---
+++ lesson10/js/homepage.js
@@ -0,0 +1,36 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+
+        const towns = jsonObject['towns'];
+        const townCard = document.querySelector('.townCard');
+        const townCardFilter = towns.filter(town => town.name == "Preston" || town.name == "Fish Haven" || town.name == "Soda Springs");
+
+        townCardFilter.forEach(town => {
+
+            let card = document.createElement('section');
+            let div = document.createElement('div');
+            let h2 = document.createElement('h2');
+            let p1 = document.createElement('p');
+            let p2 = document.createElement('p');
+            let p3 = document.createElement('p');
+            let p4 = document.createElement('p');
+            let image = document.createElement('img');
+
+            h2.innerHTML = `${town.name}`;
+            p1.innerHTML = `${town.motto}`;
+            p2.innerHTML = `Year Founded: ${town.yearFounded}`;
+            p3.innerHTML = `Population: ${town.currentPopulation}`;
+            p4.innerHTML = `Annual Rain Fall: ${town.averageRainfall}`;
+            image.setAttribute('src', `images/` + town.photo);
+            image.setAttribute('alt', `${town.name} image`);
+            div.append(h2, p1, p2, p3, p4);
+            card.append(div, image);
+            townCard.append(card);
+        });
+    });
