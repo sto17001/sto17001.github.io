96efb88fb843d3b5302a076d216eeb59e224abc5
sto17001 <sto17001@byui.edu>
2021-03-20 14:27:31 -0600

    update

--- lesson11/js/forecastapi.js
+++ lesson11/js/forecastapi.js
@@ -17,3 +17,25 @@ fetch(apiURLforecast)
         }
 
     });
+
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const towns = jsonObject['towns'];
+
+    const prestonfilter = towns.filter(x => x.name == 'Preston');
+    let prestonEvents = prestonfilter[0].events;
+    let ul = document.createElement('ul'); 
+
+    prestonEvents.forEach(info => {
+    let listItem = document.createElement('li');
+    listItem.innerHTML = info;
+    ul.appendChild(listItem);
+  }); 
+
+  document.getElementById("prestonEvents").appendChild(ul);
+});
